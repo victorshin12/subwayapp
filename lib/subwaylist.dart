f3fdf9d2322debf0661cc03ccb65aefdfbd997fd
Victor Shin <victorshin1230@gmail.com>
2023-10-10 23:55:39 -0400

    design & background change to subwaylist

--- lib/subwaylist.dart
+++ lib/subwaylist.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:subwayapp/setalarm.dart';
 
@@ -15,45 +17,78 @@ class _SubwayListState extends State<SubwayList> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.transparent, title: const Text("지하철 목록")),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                
-              ],
-            ),
-            SubwayCard(),
-            SubwayCard(),
-            SubwayCard(),
-            SubwayCard(),
-          ],
-        )
+        leading: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black54,
+          ),
+        ),
+        title: Text(
+          "지하철 목록",
+          textAlign: TextAlign.center,
+        ),
+        backgroundColor: Colors.white,
+        surfaceTintColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Stack(children: [
+      Positioned.fill(
+        child: Image.asset(
+          'assets/subwaymap.png',
+          fit: BoxFit.cover,
+        ),
+      ),
+      BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 7, sigmaY: 7),
+          child: Container(
+            color:
+                Colors.white.withOpacity(0.5), // Adjust the opacity as needed
+            child: SingleChildScrollView(
+              child: Column(
+                      children: [
+              SubwayCard(station1: "청라국제도시", station2: "홍대입구"),
+              SubwayCard(station1: "청라국제도시", station2: "홍대입구"),
+              SubwayCard(station1: "청라국제도시", station2: "홍대입구"),
+              SubwayCard(station1: "청라국제도시", station2: "홍대입구"),
+              SubwayCard(station1: "청라국제도시", station2: "홍대입구"),
+                      ],
+                    ),
+            )
       )
-    );
+    )]));
   }
 }
 
-class SubwayCard extends StatelessWidget {
+class SubwayCard extends StatefulWidget {
+  String station1;
+  String station2;
+  SubwayCard({required this.station1, required this.station2});
+
+  @override
+  State<SubwayCard> createState() => _SubwayCardState();
+}
+
+class _SubwayCardState extends State<SubwayCard> {
   @override
   Widget build(BuildContext context) {
-    String station1 = "청라국제도시";
-    String station2 = "홍대입구";
 
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
         decoration: BoxDecoration(
-            border: Border.all(color: Colors.black),
-            borderRadius: BorderRadius.circular(8)),
+          color: Colors.white,
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(8)),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
               Align(
                 alignment: Alignment.center,
-                child: Text(station1 + " --> " + station2,
+                child: Text(widget.station1 + " → " + widget.station2,
                     style: TextStyle(fontSize: 18)),
               ),
               SizedBox(height: 10),
